e882f2684a38cbc6f69967874b77eca2be897531
David <davidm06@ucm.es>
2015-10-27 14:01:27 +0100

    belekamp en proceso

--- src/types.hpp
+++ src/types.hpp
@@ -1,8 +1,12 @@
 #ifndef __TYPES_HPP
 #define __TYPES_HPP
 
+class F;
+
 typedef long long ll;
 typedef unsigned long long ull;
 typedef std::pair<ll,ll> ll_ll;
+typedef std::vector< vector< F > > matrixF;
+typedef long long int bint;
 
 #endif // __TYPES_HPP
