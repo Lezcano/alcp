fb61b86b65374eb14566b8d65e5a4e7c80afcb29
David <davidm06@ucm.es>
2015-10-29 22:58:13 +0100

    berlekamp acabado escribir código

--- src/berlekamp.cpp
+++ src/berlekamp.cpp
@@ -3,14 +3,17 @@
 #include <algorithm>
 
 template <typename FX>
+typedef std::vector< vector< F > > matrixF;
 
 /**
  * Input: a polinomial pol over a field of size q
- * O(q n^2) where n is deg(pol)
+ * Output: Matrix Q with x^0, x^q, x^{2q},..., x^{(n-1)*q} (mod pol) as rows
+ * Complexity: O(q n^2) where n is deg(pol)
+ * There is a solution in O(log(q)n^2 + n^3)
  */
-const matrixF&& formMatrix (const FX &pol) { //TODO: change/check ALL ll
+const matrixF&& formMatrix (const FX &pol) {
 	bint q = pol.baseFieldSize();
-	int n = pol.degree(); //TODO: n es el grado de un polinomio, ¿usar int?
+	int n = pol.degree();
 	FX::F aux(0, q);
 	std::vector<F> r(n, 0);
 	r[0] = 1; //r == (1, 0, ..., 0)
@@ -28,13 +31,15 @@ const matrixF&& formMatrix (const FX &pol) { //TODO: change/check ALL ll
 	}	
 	return result;	
 }
+
 /**
  * Input: a square matrix.
- * Output: a basis for the kernel of matrix. The matrix is destroyed.
+ * Output: a basis for the kernel of a matrix. The matrix is destroyed.
  */
 const vector< vector< F > >&& kernelBasis (const matrixF & matrix){
 	bint n = matrix.size();
 	bint i, j, k;
+	vector< vector< F > > result;
 	for (k = 0; k < n; ++k ){
 		//Search for pivot element
 		for (i = k; i<n && matrix[k][i] ==0 ; ++i);
@@ -59,23 +64,45 @@ const vector< vector< F > >&& kernelBasis (const matrixF & matrix){
 				++i;
 			}
 		}
-		// M = M - I;
-		for (i=0; i<n; ++i){
-			matrix[i][i] -= 1;
-		}
-		//Return non zero rows
-		j=0;
-		while (j < n){
-			while (true){
-				for (){
-					
-				}
-			}	
-		}
 	}
+	// M = M - I;
+	for (i=0; i<n; ++i)
+		matrix[i][i] -= 1;
+	//Return non zero rows
+	j=0;
+	while (j < n){
+		//Look for the next non zero row
+		while (true){
+			if (j >=n) break;
+			for (i = 0; i < n && matrix[j][i] == 0; ++i);
+			if (i==n) ++j;
+			else break;
+		}	
+		if (j >= n) break;
+		result.push_back(matrix[j]);
+	}
+	return result; //result[0] should always be (1, 0, ... 0). (Test it!)
 }
 
 //Input: a square-free polynomial pol \in F_{p^m}[x]
-const std::vector<FX>& berlekamp_simple (const FX &pol){
-	
+const std::vector<FX>&& berlekamp_simple (const FX &pol){
+	vector<FX> factors = pol;
+	bint r;
+	matrixF matrix = formMatrix(pol);
+	for (int i=0; i<n; ++i)
+		matrix[i][i] -= 1;
+	vector< vector< F > > base = kernelBasis(matrix);
+	int k = base.size(), j; 
+	while (factors.size() < k){
+		for (int i = 0; i < factors.size(); ++i){
+			for(s \in F){ //Iterar sobre todos los elementos del cuerpo??
+				FX g = gcd(FX(base)-s, factors[i]);
+				if (g != 1 && g != factors[i]){
+					factors[i]/=g; //We continue in the loop with the new factors[i] because it is a divisor of the old factors[i] so it is not necessary to check the previous s and r.
+					factors.push_back(g); 
+				}
+			}
+			++r;
+		}
+	}
 }
--- src/types.hpp
+++ src/types.hpp
@@ -6,7 +6,6 @@ class F;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef std::pair<ll,ll> ll_ll;
-typedef std::vector< vector< F > > matrixF;
 typedef long long int bint;
 
 #endif // __TYPES_HPP
